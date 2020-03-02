70e8abd8774ae06e6dcba5b32cedb0bfce33f40b
Bugofbook <bugofbook@gmail.com>
2020-03-02 22:39:34 +0800

    add blog

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 // import './index.css';
+import Blog from "./blog/Blog";
 
 import { HashRouter, Route, Switch } from "react-router-dom";
 
@@ -10,14 +11,16 @@ import { Home,  About, Knowledge, Work, Whoops404 } from "./views/viewsIndex";
 window.React = React
 
 ReactDOM.render(
-  <HashRouter>
-    <Switch>
-      <Route  exact path="/"  component={Home}/>
-      <Route  exact path="/about"  component={About}/>
-      <Route  exact path="/knowledge"  component={Knowledge}/>
-      <Route  exact path="/work"  component={Work}/>
-      <Route component={Whoops404} />
-    </Switch>
-  </HashRouter> ,
+  // <HashRouter>
+  //   <Switch>
+  //     <Route  exact path="/"  component={Home}/>
+  //     <Route  exact path="/about"  component={About}/>
+  //     <Route  exact path="/knowledge"  component={Knowledge}/>
+  //     <Route  exact path="/work"  component={Work}/>
+  //     <Route component={Whoops404} />
+  //   </Switch>
+  // </HashRouter> ,
+  <Blog />,
+  // <App />,
   document.getElementById('root')
 );
